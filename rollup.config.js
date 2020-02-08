9721686cea9e6f46d3c7e85fa3903ca79574149b
Simone Todaro <simone.todaro@gear4music.com>
2020-02-08 20:06:21 +0000

    Separate rollup files

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import filesize from 'rollup-plugin-filesize';
 import { terser } from "rollup-plugin-terser";
 import resolve from "rollup-plugin-node-resolve"
 import stripCode from 'rollup-plugin-strip-code';
-import commonjs from 'rollup-plugin-commonjs';
 
 
 export default [{
@@ -29,48 +28,6 @@ export default [{
             stripCode({
                 start_comment: 'IE11-ONLY:START',
                 end_comment: 'IE11-ONLY:END'
-            })
-        ]
-    },
-    {
-        input: 'src/index.js',
-        output: {
-            name: 'Alpine',
-            file: 'dist/alpine-ie11.js',
-            format: 'umd',
-            sourcemap: true,
-        },
-        plugins: [
-            commonjs(),
-            resolve(),
-            filesize(),
-            terser({
-                    mangle: false,
-                    compress: {
-                        drop_debugger: false,
-                    }
-            }),
-            babel({
-                babelrc: false,
-                exclude: 'node_modules/**',
-                presets: [
-                    [
-                        "@babel/preset-env",
-                        {
-                            targets: {
-                                browsers: "> 0.5%, ie >= 11"
-                            },
-                            modules: false,
-                            spec: true,
-                            useBuiltIns: "usage",
-                            forceAllTransforms: true,
-                            corejs: {
-                                version: 3,
-                                proposals: false
-                            }
-                        }
-                    ]
-                ]
             })
         ]
     }
