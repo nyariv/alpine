9e3d11c499a861d28716c65c36e6fca57213bca6
nyariv <nyariv@gmail.com>
2020-04-11 22:23:06 +0300

    Cache for safer eval no return

--- src/utils.js
+++ src/utils.js
@@ -72,16 +72,14 @@ export function saferEval(expression, dataContext, additionalHelperVariables = {
 }
 
 export function saferEvalNoReturn(expression, dataContext, additionalHelperVariables = {}) {
-    const code = `${expression}`
-    const allowedGlobals = Sandbox.SAFE_GLOBALS
-    const allowedPrototypes = Sandbox.SAFE_PROTOTYPES
-    allowedPrototypes.set(CustomEvent, [])
-    const sandbox = new Sandbox(allowedGlobals, allowedPrototypes)
+    const code = `${expression}`;
+    const codeRet = `return ${expression};`;
 
     // For the cases when users pass only a function reference to the caller: `x-on:click="foo"`
     // Where "foo" is a function. Also, we'll pass the function the event instance when we call it.
     if (Object.keys(dataContext).includes(expression)) {
-        const exec = sandbox.compile('return ' + code)
+        const exec = expressionCache[codeRet] || sandbox.compile(codeRet);
+        expressionCache[codeRet] = exec;
         const methodReference = exec(dataContext, additionalHelperVariables)
         if (typeof methodReference === 'function') {
             return methodReference.call(dataContext, additionalHelperVariables['$event'])
@@ -89,7 +87,8 @@ export function saferEvalNoReturn(expression, dataContext, additionalHelperVaria
         return methodReference
     }
 
-    const exec = sandbox.compile(code)
+    const exec = expressionCache[code] || sandbox.compile(code)
+    expressionCache[code] = exec;
     return exec(dataContext, additionalHelperVariables)
 }
 
